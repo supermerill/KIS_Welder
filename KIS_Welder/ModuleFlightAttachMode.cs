0cf8fc4f73793cf6c6316c3ce202891fcc8fb2ba
meril <merill@free.fr>
2015-06-28 16:46:17 +0300

    first commit

---
+++ KIS_Welder/ModuleFlightAttachMode.cs
@@ -0,0 +1,77 @@
+﻿
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Collections;
+using System.Text;
+using UnityEngine;
+
+namespace KIS_Welder
+{
+
+
+	/**
+	 * This class can be add to a part to add some info about rule for attach/detach (screw vs weld)
+	 * This name is defined as is instead of "ModuleKISAttachMode" to have a better info name.
+	 * */
+	public class ModuleAttachMode : PartModule
+	{
+		//used to know if this part is welded or screwed tohis parent (ignored if !canBeScrewed)
+		// Maybe add onguiactive= true to show this information to the player.
+		//default : screwed
+		[KSPField(isPersistant = true)]
+		public bool isWelded = false;
+
+		//set true if this part can receive / can be attache via screws
+		[KSPField]
+		public bool canBeScrewed = false;
+
+		//set true if this part can be weld 
+		// if false and canBeScrewed false => it can't be attached/detach in flight 
+		//        => part too complex to do that in flight (like for engine)
+		[KSPField]
+		public bool canBeWeld = true;
+
+		//to show what can be done with it on the 
+		public override string GetInfo()
+		{
+			string info = "";
+			if (canBeScrewed)
+			{
+				info += "Can be screwed";
+			}
+			if (canBeWeld)
+			{
+				info += (info.Length > 0 ? "\n" : "") + "Can be weld";
+			}
+			if (info.Length == 0)
+			{
+				info += "Can't be attach in flight";
+			}
+			return info;
+		}
+
+		public override void OnStart(PartModule.StartState state)
+		{
+			base.OnStart(state);
+			if (state == StartState.PreLaunch)
+			{
+				if (!this.canBeScrewed) this.isWelded = true;
+				else if (part.parent.Modules.Contains("ModuleAttachMode"))
+				{
+					ModuleAttachMode parentMod = part.parent.Modules["ModuleAttachMode"] as ModuleAttachMode;
+					if (parentMod.canBeScrewed)
+					{
+						//this piece in screwed in the vab if the parent and this can be screwed together.
+						this.isWelded = false;
+					}
+				}
+				else
+				{
+					//this part can be screwed and the parent has no information => ok for screwing.
+					this.isWelded = false;
+				}
+			}
+		}
+	}
+}
---
+++ KIS_Welder/ModuleKISScrewTool.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using KIS;
+
+namespace KIS_Welder
+{
+	public class ModuleKISScrewTool : ModuleKISItemAttachTool
+	{
+
+		[KSPField]
+		public bool isWeldingTool = false; // false => screw tool
+
+		// check if the pointed part can be attach with our current tool
+		public override void OnItemUse(KIS_Item item, KIS_Item.UseFrom useFrom)
+		{
+			// Check if grab key is pressed
+			if (useFrom == KIS_Item.UseFrom.KeyDown)
+			{
+				if (!KISAddonPickup.draggedPart && !KISAddonPickup.instance.grabActive && !KISAddonPointer.isRunning)
+				{
+					item.PlaySound(KIS_Shared.bipWrongSndPath);
+					ScreenMessages.PostScreenMessage("Use this tool while in drop mode to attach / Use grab key to detach", 5, ScreenMessageStyle.UPPER_CENTER);
+				}
+				if (KISAddonPointer.isRunning && KISAddonPointer.pointerTarget != KISAddonPointer.PointerTarget.PartMount)
+				{
+					float attachPartMass = KISAddonPointer.partToAttach.mass + KISAddonPointer.partToAttach.GetResourceMass();
+					if (attachPartMass < attachMaxMass)
+					{
+						//test if the tool can attach this part (screw or weld)
+                        //default (when ModuleAttachMode is not here) : magical yes
+						bool testIfCanAttachPart = true;
+						if (KISAddonPointer.partToAttach.Modules.Contains("ModuleAttachMode"))
+						{
+							ModuleAttachMode mkpam = (KISAddonPointer.partToAttach.Modules["ModuleAttachMode"] as ModuleAttachMode);
+							if (!mkpam.canBeWeld && !mkpam.canBeScrewed)
+							{
+								ScreenMessages.PostScreenMessage("This part can't be attached", 5, ScreenMessageStyle.UPPER_CENTER);
+								testIfCanAttachPart = false;
+							}
+							else
+							{
+								testIfCanAttachPart = isWeldingTool ? mkpam.canBeWeld : mkpam.canBeScrewed;
+								item.PlaySound(KIS_Shared.bipWrongSndPath);
+								if (!testIfCanAttachPart)
+								{
+									ScreenMessages.PostScreenMessage("This part can't be attached with this tool: it need a " +
+										(isWeldingTool ? "screwdriver" : "weld tool"), 5, ScreenMessageStyle.UPPER_CENTER);
+								}
+							}
+						}
+						if (testIfCanAttachPart)
+						{
+							KISAddonPickup.instance.pointerMode = KISAddonPickup.PointerMode.Attach;
+							KISAddonPointer.allowStack = allowStack;
+							item.PlaySound(changeModeSndPath);
+						}
+					}
+					else
+					{
+						item.PlaySound(KIS_Shared.bipWrongSndPath);
+						ScreenMessages.PostScreenMessage("This part is too heavy for this tool", 5, ScreenMessageStyle.UPPER_CENTER);
+					}
+				}
+
+			}
+			if (useFrom == KIS_Item.UseFrom.KeyUp)
+			{
+				if (KISAddonPointer.isRunning && KISAddonPickup.instance.pointerMode == KISAddonPickup.PointerMode.Attach)
+				{
+					KISAddonPickup.instance.pointerMode = KISAddonPickup.PointerMode.Drop;
+					KISAddonPointer.allowStack = false;
+					item.PlaySound(changeModeSndPath);
+				}
+			}
+
+		}
+
+		// to detach something it need to be screwed and we need a screwing tool
+		public override bool OnCheckDetach(Part partToDetach, ref string[] errorMsg)
+		{
+			if (!base.OnCheckDetach(partToDetach, ref errorMsg))
+			{
+				return false;
+			}
+            //Debug.Log("OnCheckDetach2 " + (partToDetach == null ? "null" : partToDetach.name)
+            //    + " =parent=> "+(partToDetach.parent == null ? "null" : partToDetach.parent.name)
+            //    + " & has mod? " + (partToDetach.Modules == null));
+			if (partToDetach.Modules == null) return true;
+			// Check if part can be detached from parent with this tool
+            //Debug.Log("OnCheckDetach3 " + partToDetach.Modules.Contains("ModuleAttachMode"));
+			if (partToDetach.parent && partToDetach.Modules.Contains("ModuleAttachMode"))
+			{
+				ModuleAttachMode mkpam = (partToDetach.Modules["ModuleAttachMode"] as ModuleAttachMode);
+                //Debug.Log("OnCheckDetach4.1 " + isWeldingTool + ", " + mkpam.canBeScrewed + ", " + mkpam.canBeWeld + " : " + mkpam.isWelded);
+				if (!mkpam.canBeWeld && !mkpam.canBeScrewed)
+				{
+					errorMsg = new string[] { "KIS/Textures/forbidden", "Can't grab", "(Part can't be detached without a tool" };
+					return false;
+				}
+				if (isWeldingTool && !mkpam.canBeWeld)
+				{
+					errorMsg = new string[] { "KIS/Textures/forbidden", "Can't grab", "(Part can't be detached without a screwdriver" };
+					return false;
+				}
+				if (mkpam.isWelded || !mkpam.canBeScrewed)
+				{
+					errorMsg = new string[] { "KIS/Textures/forbidden", "Can't grab", "(Part can't be detached : it's welded" };
+					return false;
+				}
+			}
+			//if can't find the module => don't check, open bar!
+			return true;
+		}
+
+		// apply the screw/weld property
+        public override void OnAttachToolUsed(Part srcPart, Part tgtPart, KISAttachType moveType, KISAddonPointer.PointerTarget pointerTarget)
+		{
+            base.OnAttachToolUsed(srcPart, tgtPart, moveType, pointerTarget);
+            //Debug.Log("OnItemMove2 " + (srcPart == null ? "null" : srcPart.name) + " => " +
+            //    (tgtPart == null ? "null" : tgtPart.name) + ", " + moveType + ", " + pointerTarget);
+			//set welded if needed
+            if ((moveType == KISAttachType.ATTACH)
+				&& srcPart.Modules.Contains("ModuleAttachMode")
+				)
+			{
+				ModuleAttachMode mkpam = srcPart.Modules["ModuleAttachMode"] as ModuleAttachMode;
+				mkpam.isWelded = isWeldingTool;
+                //Debug.Log("OnItemMove3 " + isWeldingTool);
+			}
+		}
+
+		//to attach, the receiver (parent) parts need the same canWeld proty than the tool (children part already check in OnItemUse)
+		protected override bool OnCheckAttach(Part srcPart, Part tgtPart, ref string toolInvalidMsg)
+		{
+			if (!base.OnCheckAttach(srcPart, tgtPart, ref toolInvalidMsg)) return false;
+			//default (when ModuleAttachMode is not here) : magical yes
+			bool cannotAttach = false;
+			bool wrongTool = false;
+			if (srcPart.Modules.Contains("ModuleAttachMode"))
+			{
+				ModuleAttachMode mkpam = (srcPart.Modules["ModuleAttachMode"] as ModuleAttachMode);
+				cannotAttach = !mkpam.canBeWeld && !mkpam.canBeScrewed;
+				if (!cannotAttach) wrongTool = isWeldingTool ? !mkpam.canBeWeld : !mkpam.canBeScrewed;
+			}
+			//Debug.Log("OnCheckAttach2 " + (srcPart == null ? "null" : srcPart.name) + " => " +
+			//	(tgtPart == null ? "null" : tgtPart.name) + ", " + wrongTool + ", " + cannotAttach);
+			if (!wrongTool && !cannotAttach)
+			{
+				return true;
+			}
+			else
+			{
+				if (cannotAttach) toolInvalidMsg = ("Target part do not allow attach !");
+				else toolInvalidMsg = "Target part need a "
+						+ (isWeldingTool ? "screwdriver" : "welding tool") + " to attach something on it !";
+				return false;
+			}
+		}
+
+	}
+}
